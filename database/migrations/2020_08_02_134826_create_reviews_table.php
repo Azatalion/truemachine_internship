9ec6763e38e1cad325d59ff60f6938ae90ca8f69
azatalion <azatalion@gmail.com>
2020-08-08 17:49:42 +0400

    foreign key user_id

--- database/migrations/2020_08_02_134826_create_reviews_table.php
+++ database/migrations/2020_08_02_134826_create_reviews_table.php
@@ -18,10 +18,7 @@ class CreateReviewsTable extends Migration
             $table->text('text');
             $table->smallInteger('mark');
             $table->bigInteger('user_id')->unsigned();
-<<<<<<< HEAD
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
-=======
->>>>>>> a8dcc121cddb7b5323711d5c9596db647051298e
             $table->bigInteger('product_id')->unsigned();
             $table->foreign('product_id')->references('id')->on('products')->onDelete('cascade');
             $table->timestamps();
